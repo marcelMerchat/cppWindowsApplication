aab4714ffcfb77a38be8f031cbfcba5569fe8a92
Marcel J Merchat <merchat77@gmail.com>
2017-04-14 10:07:12 -0500

    First Version:
    Here are the files for a basic C++ Windows Application.

---
+++ MyForm.cpp
@@ -0,0 +1,16 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThread]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	RS232::MyForm form;  //RandomNumberGenerator
+	Application::Run(%form);
+}
+
